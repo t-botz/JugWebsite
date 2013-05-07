a18b5bdaa78861a00e149020285efe433ba07d5a
Tibo Delor <tibo@invalidcodeexception.com>
2013-05-07 23:57:26 +1000

    - Font for SJC changed  - Logos provided byt ZeroTurnaround and Genuitec  - Templating system

--- app/com/sjc/website/controllers/Application.scala
+++ app/com/sjc/website/controllers/Application.scala
@@ -1,11 +1,21 @@
 package com.sjc.website.controllers
 
 import play.api.mvc._
+import com.sjc.website.views.html._
 
 object Application extends Controller {
   
   def index = Action {
-    Ok(com.sjc.website.views.html.index())
+    Ok(template(home()))
   }
-  
+  def about = Action {
+    Ok(template(home()))
+  }
+  def events = Action {
+    Ok(template(home()))
+  }
+  def members = TODO
+
+  def quizz = TODO
+
 }
